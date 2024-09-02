1c5eeda1bc8b3e34d0c5df9998e7bc7d4b13d21a
satoki <adao1994saru@yahoo.co.jp>
2024-09-02 13:52:03 +0900

    ギャラリーでインスタの画面を表示させたいが、うまくいかない

--- home/views.py
+++ home/views.py
@@ -1,4 +1,16 @@
+import requests
 from django.shortcuts import render
 
+def get_instagram_posts():
+    access_token = 'IGQWRONjNkTHhMRWVFeTZAONm8ySjVDeVphVmJwMDM4QUxMYnFlZAWVXLXRqUG9GWUdNaERtcF9vWUsxemtuUWVpU3k0S2J4YWxOVWN2UmQxRjIzRE9FMjdRZAkIyaTRBRmNjZA1FnOW9JUVdLdU9xYXNpeDFWUmowNFkZD'  # Instagram APIのアクセストークン
+    user_id = '17841407386127966'  # InstagramのユーザーID
+    url = f'https://graph.instagram.com/{user_id}/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token={access_token}'
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get('data', [])
+    return []
+
 def home(request):
-  return render(request, 'home/home.html')
+    instagram_posts = get_instagram_posts()
+    return render(request, 'home.html', {'instagram_posts': instagram_posts})
