82e2d5852b800b5c4c834e05d765ae97cd3a32e2
satoki <adao1994saru@yahoo.co.jp>
2024-09-09 08:12:58 +0900

    home.htmlの header.htmlでエラー発生

--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
+from django.urls import path
 from django.shortcuts import render
-from .models import News
+from .models import News  
 
+# news関数を定義
 def news(request):
     latest_news = News.objects.all().order_by('-published_date')[:5]  # 最新5件を取得
-    return render(request, 'home/home.html', {'latest_news': latest_news})
+    return render(request, 'news/news.html', {'latest_news': latest_news})  
+
+# URLパターンを定義
+urlpatterns = [
+    path('', news, name='news'),  
+]
