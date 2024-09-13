7aaa7820aba0a29d3793dc4e34b4fef511143919
satoki <adao1994saru@yahoo.co.jp>
2024-09-14 01:05:53 +0900

    ギャラリー以外のページ変移ができたことを確認、今後は各ページのスタイルを適用させる

--- news/views.py
+++ news/views.py
@@ -3,10 +3,10 @@ from django.core.paginator import Paginator
 from .models import News
 
 def news_view(request):
-    news_list = News.objects.all().order_by('-date')
+    news_list = News.objects.all().order_by('published_date')
     paginator = Paginator(news_list, 5)  # 1ページに5件表示
 
     page_number = request.GET.get('page')
     news_list = paginator.get_page(page_number)
 
-    return render(request, 'news.html', {'news_list': news_list})
+    return render(request, 'news/news.html', {'news_list': news_list})
