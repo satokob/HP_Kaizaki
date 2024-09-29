bce369c3bf37ac68aa18e93e2abf78f143cc2b05
satoki <adao1994saru@yahoo.co.jp>
2024-09-29 22:17:42 +0900

    news と plan のスタイルが適応されない

--- gallery/instagram_api.py
+++ gallery/instagram_api.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_instagram_posts(max_items=9):
+def get_instagram_posts(max_items=18):
     access_token = 'EAAWP0w6t9cMBO2ZCAHEkZA0FE42ZBOivvzr5QYnP2Tr1GXBRU5ZBtUb3KOjUkZAnZBV7f6AMcoeAC0NmxhTyJfiTWDAXPVZCMo2YBeb42BaBaYNnFrTkuBkcnXBnTF0BDOYw3ggttkrs6SFQemsDU1ndBWpHNTz0D5wLLfoqb7orCfRFFNFKYvWzQEgSuXoRgZDZD'
     business_account_id = '17841407386127966'
     url = f'https://graph.facebook.com/v20.0/{business_account_id}/media?fields=id,caption,media_type,media_url,permalink,timestamp,username&access_token={access_token}'
